5a3ca9e1da23606406d5f455dede6f1c81923918
FNandoG <78455299+FNandoG@users.noreply.github.com>
2021-05-05 21:51:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ io.on('connection', socket => {
   socket.on('disconnect', () => {
     const user = userLeave(socket.id);
 
-    if (user.username) {
+    if (user) {
       io.to(user.room).emit(
         'message',
         formatMessage(Admin, `${user.username} has left the chat`)
